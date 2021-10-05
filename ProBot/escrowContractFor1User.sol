ccc1ff5235fc75c0555bea2370b791bf2feb3190
Timekiller7 <69202673+Timekiller7@users.noreply.github.com>
2021-10-05 15:23:58 +0300

    edited for another project, as example

--- ProBot/escrowContractFor1User.sol
+++ ProBot/escrowContractFor1User.sol
@@ -1,87 +1,52 @@
+pragma solidity 0.8.7;      
 
-pragma solidity 0.6.0;      
-   
 
-contract escrow{ 
-  
-
+contract Escrow { 
     address payable public buyer; 
     address payable public seller; 
-    address payable public arbiter; 
+    address public arbiter; 
     mapping(address => uint) TotalAmount; 
-  
- 
-    enum State{                                                //стадии
-         
-  
-        awate_payment, awate_delivery, complete                   
-    } 
-  
-  
     State public state; 
+ 
+    enum State { awate_payment, awate_delivery, complete }                                                
       
-      
-    modifier instate(State expected_state){ 
-          
+    modifier instate(State expected_state) { 
         require(state == expected_state); 
         _; 
     } 
-  
 
-    modifier onlyBuyer(){ 
-        require(msg.sender == buyer ||  
-                msg.sender == arbiter); 
+    modifier instate2(State expected_state) { 
+        require(state == expected_state || state == State.complete); 
+        _; 
+    } 
+
+    modifier onlyBuyer() { 
+        require(msg.sender == buyer || msg.sender == arbiter);  
         _; 
     } 
   
-
-    modifier onlySeller(){ 
+    modifier onlySeller() { 
         require(msg.sender == seller); 
         _; 
     } 
       
- 
-    constructor(address payable _buyer,  
-                address payable _sender) public{                      //конструктор
-        
-        // Assigning the values of the  
-        // state variables 
+    constructor(address payable _buyer, address payable _sender) {                  
         arbiter = msg.sender; 
         buyer = _buyer; 
         seller = _sender; 
         state = State.awate_payment; 
     } 
-      
-
-    function confirm_payment() onlyBuyer instate(         //самая первая транзакция покупателем
-      State.awate_payment) public payable{ 
-      
+ 
+    function pay() onlyBuyer instate2(State.awate_payment) public payable {         
         state = State.awate_delivery; 
-          
     } 
-    
-     function confirm_payment_many() onlyBuyer instate(           //все последующие,чтобы новый контракт не делать и избежать дабл траты
-      State.complete) public payable{  
-      
-        state = State.awate_delivery; 
-          
-    } 
-    
-      
 
-    function confirm_Delivery() onlyBuyer instate( 
-      State.awate_delivery) public{ 
-          
+    function deliverToSeller() onlyBuyer instate(State.awate_delivery) public { 
         seller.transfer(address(this).balance); 
         state = State.complete; 
     } 
   
-
-    function ReturnPayment() onlySeller instate( 
-      State.awate_delivery)public{ 
-        
-         
+    function returnPayment() onlySeller instate(State.awate_delivery) public {  
        buyer.transfer(address(this).balance); 
-    } 
-      
+    }    
 } 
